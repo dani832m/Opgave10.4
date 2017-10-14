3320a47a5bdd364ed77c4e64263bdc3bd3f3455d
dani832m <31951102+dani832m@users.noreply.github.com>
2017-10-14 22:35:05 +0200

    Afsæt punkter i et koordinatsystem og udregn distancen mellem disse.

---
+++ src/dk/n4/MyPoint.java
@@ -0,0 +1,98 @@
+package dk.n4;
+
+public class MyPoint {
+
+	    private double x, y;
+
+	    // Getters
+	    public double getXCoordinate() { // Returnerer x-koordinaten
+	        return x;
+        }
+
+        public double getYCoordinate() { // Returnerer y-koordinaten
+            return y;
+        }
+
+        // Setters
+        public void setXCoordinate(double x) { // Angiv x-koordinaten
+	        this.x = x;
+        }
+
+        public void setYCoordinate(double y) { // Angiv y-koordinaten
+            this.y = y;
+        }
+
+        // Methods (De fleste af metoderne herunder måtte jeg finde hjælp til på nettet)
+        public double distance(double x, double y) { // Metode tager argumenter ind
+            return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y)); // Returnerer x * x + y * y
+        }
+
+        public double distance(MyPoint point) { // Metode tager argument ind fra MyPoint med navnet point
+            return distance(point.x, point.y);
+        }
+
+        public MyPoint getCenterPoint(MyPoint p) {
+            return new MyPoint((p.x + this.x) / 2, (p.y + this.y) / 2);
+        }
+
+        public static MyPoint getCenterPoint(double x1, double y1, double x2, double y2) {
+            return new MyPoint((x1 + x2) / 2, (y1 + y2) / 2);
+        }
+
+        /** Return true if this point is on the left side of the directed line from p0 to p1 */
+        public boolean leftOfTheLine(MyPoint p0, MyPoint p1) {
+            return leftOfTheLine(p0.x, p0.y, p1.x, p1.y, x, y);
+        }
+
+        /** Return true if this point is on the same line from p0 to p1 */
+        public boolean onTheSameLine(MyPoint p0, MyPoint p1) {
+            return onTheSameLine(p0.x, p0.y, p1.x, p1.y, x, y);
+        }
+
+        /** Return true if this point is on the right side of the directed line from p0 to p1 */
+        public boolean rightOfTheLine(MyPoint p0, MyPoint p1) {
+            return rightOfTheLine(p0.x, p0.y, p1.x, p1.y, x, y);
+        }
+
+        /** Return true if this point is on the line segment from p0 to p1 */
+        public boolean onTheLineSegment(MyPoint p0, MyPoint p1) {
+            return onTheLineSegment(p0.x, p0.y, p1.x, p1.y, x, y);
+        }
+
+        /** Return true if point (x2, y2) is on the left side of the directed line from (x0, y0) to (x1, y1) */
+        public static boolean leftOfTheLine(double x0, double y0, double x1, double y1, double x2, double y2){
+            return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) > 0;
+        }
+
+        /** Return true if point (x2, y2) is on the same line from (x0, y0) to (x1, y1) */
+        public static boolean onTheSameLine(double x0, double y0, double x1, double y1, double x2, double y2) {
+            return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) == 0;
+        }
+
+        /** Return true if point (x2, y2) is on the line segment from (x0, y0) to (x1, y1) */
+        public static boolean onTheLineSegment(double x0, double y0, double x1, double y1, double x2, double y2) {
+            double position = (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0);
+            return position <= 0.0000000001 && ((x0 <= x2 && x2 <= x1) || (x0 >= x2 && x2 >= x1));
+        }
+
+        /** Return true if point (x2, y2) is on the right side of the directed line from (x0, y0) to (x1, y1) */
+        public static boolean rightOfTheLine(double x0, double y0, double x1, double y1, double x2, double y2){
+            return (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) < 0;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+
+        // Constructors
+        MyPoint() { // Default constructor der sætter koordinatsættet til (0.0, 0.0)
+            this(0,0);
+        }
+
+        MyPoint(double x, double y) { // Constructor der tager to argumenter ind, en x- og en y-koordinat
+	        this.x = x;
+	        this.y = y;
+        }
+
+}
---
+++ src/dk/n4/TestMyPoint.java
@@ -0,0 +1,18 @@
+package dk.n4;
+
+public class TestMyPoint {
+
+    public static void main(String[] args) {
+
+        MyPoint point1 = new MyPoint(); // Opretter nyt objekt (punkt) ud fra klassen MyPoint
+        MyPoint point2 = new MyPoint(2.5,3.1); // Opretter nyt objekt (punkt) ud fra klassen MyPoint
+
+        System.out.println("(" + point1.getXCoordinate() + ", " + point1.getYCoordinate() + ")"); // Printer koordinatsættet fra point1
+        System.out.println("(" + point2.getXCoordinate() + ", " + point2.getYCoordinate() + ")"); // Printer koordinatsættet fra point2
+
+        System.out.println("\nDistancen mellem de to punkter: " + point1.distance(point2)); // Printer distancen mellem point1 og point2
+        System.out.println("Distancen mellem de to punkter: " + point1.distance(2, 0)); // Printer distancen mellem point1 og indtastning
+
+    }
+
+}
